ce399d5f32f38674b0b6a1804e6662621120a477
LuizzFerreira <luizgabrielrochaferreira13@gmail.com>
2026-04-29 17:30:13 -0300

    primeiro commit3

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -15,19 +15,19 @@ export default function Social() {
       onMouseLeave={() => setActive(false)}
     >
       <div className="social-options">
-        <a href="https://wa.me/5597984160533" className="social-item whatsapp" target="_blank" rel="noreferrer">
-            <img className="WpLogo" src={WpLogo} alt="SNACK Logo" />
+        <a href="https://wa.me/5511973262573" className="social-item whatsapp" target="_blank" rel="noreferrer">
+            <img className="WpLogo" src={WpLogo} alt="Clinica Logo" />
         </a>
-        <a href="https://www.instagram.com/snackburguer_apui" className="social-item instagram" target="_blank" rel="noreferrer">
-          <img className="InstaLogo" src={InstaLogo} alt="SNACK Logo" />
+        <a href="https://www.instagram.com/drbrunoalexs?utm_source=qr" className="social-item instagram" target="_blank" rel="noreferrer">
+          <img className="InstaLogo" src={InstaLogo} alt="Clinica Logo" />
         </a>
-        <a href="tel:+5597984160533" className="social-item phone">
-          <img className="TlLogo" src={TlLogo} alt="SNACK Logo" />
+        <a href="tel:+5511973262573" className="social-item phone">
+          <img className="TlLogo" src={TlLogo} alt="Clinica Logo" />
         </a>
       </div>
       
       <button className="social-main-btn">
-        <img className="LgLogo" src={LgLogo} alt="SNACK Logo" />
+        <img className="LgLogo" src={LgLogo} alt="Clinica Logo" />
       </button>
     </div>
   );
